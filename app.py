abc92c9dcac0a520ca577ed686df61a15224183a
Sachi Angle <sacangle@users.noreply.huggingface.co>
2023-08-27 17:18:59 +0000

    multi-turn basic explore refine

--- app.py
+++ app.py
@@ -9,10 +9,19 @@ from langchain.prompts.chat import (
 )
 from langchain.schema import AIMessage, HumanMessage, SystemMessage
 
-def load_response(question):
+RESPONSE_LIMIT = 3
+REFINE_KEY = 0
+EXPLORE_KEY = 1
+INDUSTRY = "fashion"
+PURPOSE = "recommends combinations of clothes"
+
+SEARCH_SYSTEM_PROMPT = "You are a helpful assistant on an e-commerce " + INDUSTRY + " site that " + PURPOSE + " based on the human's query. Limit yourself to " + str(RESPONSE_LIMIT) + " recommendations at a time, where each recommendation is a sentence describing the recommendation."
+REFINE_SYSTEM_PROMPT = SEARCH_SYSTEM_PROMPT + "The customer will give two queries separated by \n, the second will be a refined version of the first. Use the second to make adjustments to the first, and provide a response to the final resultant query."
+
+def load_response(question, systemPrompt):
     llm = ChatOpenAI()
     messages = [
-        SystemMessage(content="You are a helpful assistant on an e-commerce fashion site that recommends combinations of outfits and clothes based on the human's query. Limit yourself to three recommendations at a time, where each recommendation is a sentence describing the recommendation."),
+        SystemMessage(content=systemPrompt),
         HumanMessage(content=question)
     ]
     response = llm(messages)
@@ -24,26 +33,63 @@ def get_text(key):
 
 def write_response(rawResponse):
     response = rawResponse.content.splitlines()
-    for idx, item in enumerate(response):
-        format_response_with_explore_refine(item, idx)
 
-def format_response_with_explore_refine(responseItem, index):
+    st.write(response[0])
+    refine_0 = st.button('Refine', key = 'refine0')
+    explore_0 = st.button('Explore', key = 'explore0')
+
+    st.write(response[1])
+    refine_1 = st.button('Refine', key = 'refine1')
+    explore_1 = st.button('Explore', key = 'explore1')
+
+    st.write(response[2])
+    refine_2 = st.button('Refine', key = 'refine2')
+    explore_2 = st.button('Explore', key = 'explore2')
+
+    while(True):
+        if refine_0 :
+            return [REFINE_KEY, response[0]]
+        else if explore_0 : 
+            return [EXPLORE_KEY, response[0]]
+        else if refine_1 :
+            return [REFINE_KEY, response[1]]
+        else if explore_1 : 
+            return [EXPLORE_KEY, response[1]]
+        else if refine_2 :
+            return [REFINE_KEY, response[2]]
+        else if explore_2 : 
+            return [EXPLORE_KEY, response[2]]
+
+'''def format_response_with_explore_refine(responseItem, index):
     st.write(responseItem)
     refine = st.button('Refine', key = 'refine'+str(index))
     explore = st.button('Explore', key = 'explore'+str(index))
 
+def check_for_clicks(responseCount):
+    for i in range(responseCount):
+        if '''
+    
 
 st.set_page_config(page_title="Refined Search", page_icon=":robot:")
 st.header("Refined Search")
 
 user_input=get_text("search1")
-response = load_response(user_input)
+response = load_response(user_input, SEARCH_SYSTEM_PROMPT)
 
 search = st.button('Search')
 
 if search:
-    write_response(response)
-    
+    nextAction = write_response(response)
+    while nextAction[0] == REFINE_KEY:
+        user_input = get_text("refine")
+        refine_query = nextAction[1] + "\n" + user_input
+        response = load_response(refine_query, REFINE_SYSTEM_PROMPT)
+        nextAction = write_response(response)
+
+    if nextAction[1] == EXPLORE_KEY:
+        st.header("Now exploring: "+ nextAction[1])
+        
+        
 
     
 
